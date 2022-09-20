048baaf20432e18d1d4339b6236fa795b0a8a7ef
David Genesis Pedeglorio <pedegloriodavidgenesis@gmail.com>
2022-09-20 23:20:47 +0800

    Add data and finish card layout

--- my-travel-journal/src/App.jsx
+++ my-travel-journal/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Card from './Card'
 import Navbar from './Navbar'
+import data from './data'
 
 function App() {
 
--- my-travel-journal/src/Card.jsx
+++ my-travel-journal/src/Card.jsx
@@ -1,9 +1,19 @@
-import data from './data'
-
 export default function Card() {
     return (
         <main>
-            
+            <div className='card'>
+                <img src="https://source.unsplash.com/WLxQvbMyfas" alt="Mount Fugi" className='card-pic'/>
+                <div class="side-info">
+                    <p className='location'>
+                        <img src="src/assets/marker.png" className='marker'/> JAPAN 
+                        <a href="https://goo.gl/maps/1DGM5WrWnATgkSNB8" target="_blank">View on Google Maps</a>
+                    </p>
+                    <h1 className='title'>Mount Fuji</h1>
+                    <strong>12 Jan, 2021 - 24 Jan, 2021</strong>
+                    <p className='description'>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                </div>
+            </div>
+            <div className='divider'></div>
         </main>
     )
 }
